d2f06194e8908230c13d119e7941274e02613457
Vladimir Čolović <vladimir.colovic@redbox.com>
2020-11-23 04:11:16 +0100

    fix

--- start.sh
+++ start.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-REPO=$REPO
-TOKEN=$TOKEN
-
 cd /home/docker/actions-runner
 
-./config.sh --url https://github.com/${REPO} --token ${TOKEN}
+./config.sh --url https://github.com/$REPO --token $TOKEN
 
 cleanup() {
     echo "Removing runner..."
-    ./config.sh remove --unattended --token ${TOKEN}
+    ./config.sh remove --unattended --token $TOKEN
 }
 
 trap 'cleanup; exit 130' INT
