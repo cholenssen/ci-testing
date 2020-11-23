eb59fd6f01b540e6b554961fc78b5d065e11a025
Vladimir Čolović <vladimir.colovic@redbox.com>
2020-11-23 14:00:14 +0100

    sdfd

--- Dockerfile
+++ Dockerfile
@@ -15,7 +15,6 @@ RUN apt-get install -y curl jq build-essential libssl-dev libffi-dev python3 pyt
 RUN  curl -O -L https://github.com/actions/runner/releases/download/v${RUNNER_VERSION}/actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz \
     && tar xzf ./actions-runner-linux-x64-${RUNNER_VERSION}.tar.gz && ./bin/installdependencies.sh
 
-
 # copy over the start.sh script
 COPY start.sh start.sh
 
--- start.sh
+++ start.sh
@@ -6,7 +6,13 @@ ACCESS_TOKEN=$ACCESS_TOKEN
 echo  $ACCESS_TOKEN
 REG_TOKEN=$(curl -sX POST  -H "Authorization: token ${ACCESS_TOKEN}" -H 'accept: application/vnd.github.v3+json' "https://api.github.com/repos/cholenssen/ci-testing/actions/runners/registration-token" | jq .token --raw-output)
 
-./config.sh --url "https://api.github.com/repos/cholenssen/ci-testing" --token ${REG_TOKEN} --RUNNER_ALLOW_RUNASROOT 1
+if [[ -n "${ACCESS_TOKEN}" ]]; then
+  _TOKEN=$(bash /token.sh)
+  RUNNER_TOKEN=$(echo "${_TOKEN}" | jq -r .token)
+  _SHORT_URL=$(echo "${_TOKEN}" | jq -r .short_url)
+fi
+
+./config.sh --url "https://api.github.com/repos/cholenssen/ci-testing" --token "${REG_TOKEN}"
 
 cleanup() {
     echo "Removing runner..."
